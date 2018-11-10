fb0f7aac2d09116207fd9eabf3d82fdf24ed59e2
Darius Goore <dariusgoore@gmail.com>
2018-11-09 19:09:26 -0500

    app currently works at the end of part 1
    posts displays hard coded data from src/app.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
     }
   ]
 })
